0997dc3d5a43d645f9d9402921438522219aa24b
Ignacio Rabanal <farm850@gmail.com>
2026-03-08 01:21:04 -0300

    fix: bind server to 0.0.0.0 for railway

--- server/index.ts
+++ server/index.ts
@@ -91,13 +91,13 @@ app.use((req, res, next) => {
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
 
-httpServer.listen(
-  {
-    port,
-    host: "127.0.0.1",
-  },
-  () => {
-    log(`serving on port ${port}`);
-  },
-);
+  httpServer.listen(
+    {
+      port,
+      host: "0.0.0.0",
+    },
+    () => {
+      log(`serving on port ${port}`);
+    },
+  );
 })();
